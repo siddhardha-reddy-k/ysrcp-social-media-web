d1e4439e5e87b93c0f6498d11fe30cc351d5612f
Sid <k.siddhardhareddy112@gmail.com>
2026-03-01 22:19:16 +0530

    Initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,37 @@
+import logo from "./logo-warriors-on-a-mission.jpg";
+import jaganHero from "./ys-jagan-mohan-reddy.png";
+import ysrAboutUs from "./ys-raja-sekhar-reddy.jpeg";
+import ysrcp2011 from "./party-logo-2011.jpg";
+import ysrcp2014 from "./jagan-sir-2014.jpg";
+import ysrcp2019 from "./ysrcp-2019.jpg";
+import jaganContact from "./jagan_lion.png";
+
+import shyamSripathi from "./volunteers/v1.jpg";
+import santoshBondili from "./volunteers/v2.jpg";
+import venkateshT from "./volunteers/v3.jpg";
+import ranjithKumar from "./volunteers/v4.jpg";
+import chandraMouliJ from "./volunteers/v5.jpg";
+import phanindraP from "./volunteers/v6.jpg";
+import sivaPrasadReddyP from "./volunteers/v7.jpg";
+import nagendraJ from "./volunteers/v8.jpg";
+
+export const assets = {
+  logo,
+  jaganHero,
+  ysrAboutUs,
+  ysrcp2011,
+  ysrcp2014,
+  ysrcp2019,
+  jaganContact,
+};
+
+export const volunteerData = [
+  { name: "Shyam Sripathi", img: shyamSripathi },
+  { name: "Santosh Bondili", img: santoshBondili },
+  { name: "Venkatesh T", img: venkateshT },
+  { name: "Ranjith Kumar", img: ranjithKumar },
+  { name: "Chandra Mouli J", img: chandraMouliJ },
+  { name: "Phanindra P", img: phanindraP },
+  { name: "Siva Prasad Reddy P", img: sivaPrasadReddyP },
+  { name: "Nagendra J", img: nagendraJ },
+];
